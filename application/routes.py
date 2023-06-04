8010a0e8181e7002ad81d0e5949a10b5d9a77d89
Dhanushka Sandaruwan <sandaruwandanushka@gmail.com>
2023-06-04 13:22:28 +0530

    Add URL variables

--- application/routes.py
+++ application/routes.py
@@ -7,10 +7,12 @@ def index():
     return render_template("index.html", login = True)
 
 @app.route("/courses")
-def courses():
+@app.route("/courses/")
+@app.route("/courses/<term>")
+def courses(term="Spring 2019"):
     coursesData = [{"courseID":"1111","title":"PHP 101","description":"Intro to PHP","credits":3,"term":"Fall, Spring"}, {"courseID":"2222","title":"Java 1","description":"Intro to Java Programming","credits":4,"term":"Spring"}, {"courseID":"3333","title":"Adv PHP 201","description":"Advanced PHP Programming","credits":3,"term":"Fall"}, {"courseID":"4444","title":"Angular 1","description":"Intro to Angular","credits":3,"term":"Fall, Spring"}, {"courseID":"5555","title":"Java 2","description":"Advanced Java Programming","credits":4,"term":"Fall"}]
     print(coursesData)
-    return render_template("courses.html", courseData=coursesData)
+    return render_template("courses.html", courseData=coursesData, term=term)
 
 @app.route("/")
 @app.route("/enrollment")
